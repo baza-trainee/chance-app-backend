ba725583e5c99bb528248aadef4de13f8d8877cb
Ivan Khomenko <simkav2411@gmail.com>
2024-02-16 18:34:50 +0200

    refactor remove unused code

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,7 +2,6 @@ import { UserModule } from './../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './guards/auth.guard';
 import { LocalStrategy } from './strategy/local.strategy';
 import { LocalSerializer } from './serializers/local.seriallizer';
 import { GoogleStrategy } from './strategy/google.strategy';
@@ -11,7 +10,6 @@ import { GoogleStrategy } from './strategy/google.strategy';
   controllers: [AuthController],
   providers: [
     AuthService,
-    AuthGuard,
     LocalStrategy,
     LocalSerializer,
     GoogleStrategy,
--- server/src/shared/models/location.model.ts
+++
@@ -1,9 +0,0 @@
-import { prop } from '@typegoose/typegoose';
-
-export class Location {
-  @prop()
-  type: string;
-
-  @prop({ required: true })
-  coordinates: number[];
-}
