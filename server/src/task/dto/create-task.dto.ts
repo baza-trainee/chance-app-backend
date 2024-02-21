1c64ac926bea647717ef44c85d6d7c954fbe2dbb
Ivan <46397494+Simkav@users.noreply.github.com>
2024-02-21 14:28:50 +0200

    fix validation for tasks

--- server/src/task/dto/create-task.dto.ts
+++ server/src/task/dto/create-task.dto.ts
@@ -1,11 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmptyCustom } from '../../shared/decorators/isNotEmptyCustom';
 import { IsStringCustom } from '../../shared/decorators/isStringCustom';
+import { LengthCustom } from '../../shared/decorators/LengthCustom';
 import { IsDate } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateTaskDto {
   @ApiProperty()
+  @LengthCustom(2, 300)
   @IsNotEmptyCustom()
   @IsStringCustom()
   message: string;
--- server/src/task/dto/update-task.dto.ts
+++ server/src/task/dto/update-task.dto.ts
@@ -1,11 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmptyCustom } from '../../shared/decorators/isNotEmptyCustom';
 import { IsStringCustom } from '../../shared/decorators/isStringCustom';
+import { LengthCustom } from '../../shared/decorators/LengthCustom';
 import { IsDate, IsOptional, IsBoolean } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class UpdateTaskDto {
   @ApiProperty()
+  @LengthCustom(2, 300)
   @IsNotEmptyCustom()
   @IsStringCustom()
   @IsOptional()
