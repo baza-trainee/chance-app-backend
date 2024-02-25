3eeb02632010285e1a287995d872b880dfb14428
Ivan <46397494+Simkav@users.noreply.github.com>
2024-02-25 18:01:45 +0200

    add policy route

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,Res } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   @Get()
   ping() {
     return 'pong';
+  }
+  @Get('policy')
+  getPolicy(@Res() res: Response) {
+    const file = createReadStream(join(process.cwd(),'src','static','policy.pdf'))
+    file.pipe(res)
   }
 }
