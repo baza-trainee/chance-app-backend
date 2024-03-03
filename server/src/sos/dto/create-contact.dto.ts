deb825ebcd62ea10a0c434cf8cd938e00171d524
Andrii Shtohryn <andriishtohryn@gmail.com>
2024-03-03 19:52:02 +0200

    update sos, init socket

--- server/src/sos/dto/create-contact.dto.ts
+++ server/src/sos/dto/create-contact.dto.ts
@@ -2,19 +2,37 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsStringCustom } from '../../shared/decorators/isStringCustom';
 import { LengthCustom } from '../../shared/decorators/LengthCustom';
 import { IsPhoneCustom } from '../../shared/decorators/isPhoneCustom';
-import { IsOptional } from 'class-validator';
+import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class CreateContactDto {
+class ContactDto {
   @ApiProperty()
   @IsStringCustom()
   @LengthCustom(2, 30)
   name: string;
+
   @ApiProperty()
   @IsStringCustom()
   @IsPhoneCustom()
   phone: string;
+}
+
+export class CreateContactDto {
+  // @ApiProperty()
+  // @IsStringCustom()
+  // @LengthCustom(2, 30)
+  // name: string;
+  // @ApiProperty()
+  // @IsStringCustom()
+  // @IsPhoneCustom()
+  // phone: string;
   @ApiProperty()
-  @IsStringCustom()
+  @IsString()
   @IsOptional()
   group: string;
+  @ApiProperty({ type: [ContactDto] })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ContactDto)
+  contacts: ContactDto[];
 }
